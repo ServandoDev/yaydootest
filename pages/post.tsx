c25637146693a61fa2a07c3f99893783a197efc1
ServandoDev <servando1994ser@gmail.com>
2022-08-08 11:52:09 -0500

    Update post

--- pages/post.tsx
+++ pages/post.tsx
@@ -66,53 +66,69 @@ const Post: NextPage = () => {
                 </div>
                 <div className='container-fluid p-5'>
                     <div className='row'>
-                        <div className='col-12'>
-                            <h3>Posts of {user.first_name + ' ' + user.last_name}</h3>
+                        <div className='col-xl-3 col-lg-4 col-md-12 col-sm-12 col-xs-12'>
+                            <div className='card mt-2 border-0'>
+                                <div className="card-body text-center">
+                                    <img className='rounded' src={user.avatar?.toString()} alt="avatar" />
+                                    <h5 className="card-title">
+                                        {user.first_name + ' ' + user.last_name}
+                                    </h5>
+                                    <p className="card-text">
+                                        {user.email}
+                                    </p>
+                                    <p className="card-text">
+                                        Welcome to my profile, feel free to take a look to my posts and don't forget to leave a like or a comment!
+                                    </p>
+                                    <p className="card-text">
+                                        <small className="text-muted">Last updated 3 mins ago</small>
+                                    </p>
+                                </div>
+                            </div>
                         </div>
-                    </div>
-                    <div className='row'>
-                        {
-                            posts.map((post: any) =>
-                                <div key={post.id} className='row mt-2'>
-                                    <div className='col-12'>
-                                        <div className="card w-100">
-                                            <div className="card-header d-flex justify-content-between">
-                                                <h5>{user.first_name + " " + user.last_name + ' says:'}</h5>
-                                                <button type='button' className='btn btn-outline-danger'
-                                                    onClick={() => deletePost(post)}>
-                                                    <span className='material-icons'>
-                                                        delete
-                                                    </span>
-                                                </button>
-                                            </div>
-                                            <div className="card-body">
-                                                <h5 className="card-title">
-                                                    {post.title}
-                                                </h5>
-                                                <p className="card-text">
-                                                    {post.body}
-                                                </p>
-                                                <div className="card-footer d-flex w-100 justify-content-between text-muted">
-                                                    <p>{getRandomDays()}</p>
-                                                    <div className='d-inline'>
-                                                        <div className="badge rounded-pill text-bg-secondary d-flex align-items-center">
-                                                            <span className='material-icons'>
-                                                                thumb_up
-                                                            </span>23
-                                                        </div>
-                                                        <div className="mt-1 badge rounded-pill text-bg-secondary d-flex align-items-center">
-                                                            <span className='material-icons'>
-                                                                question_answer
-                                                            </span>12
+                        <div className='col-xl-9 col-lg-8 col-md-12 col-sm-12 col-xs-12 scroll-vertical'>
+                            {
+                                posts.map((post: any) =>
+                                    <div key={post.id} className='row mt-2 border-bottom'>
+                                        <div className='col-12'>
+                                            <div className="card w-100 border-0">
+                                                <div className="card-header d-flex justify-content-between border-0">
+                                                    <h5>{user.first_name + " " + user.last_name + ' says:'}</h5>
+                                                    <button type='button' className='btn btn-outline-danger'
+                                                        onClick={() => deletePost(post)}>
+                                                        <span className='material-icons'>
+                                                            delete
+                                                        </span>
+                                                    </button>
+                                                </div>
+                                                <div className="card-body">
+                                                    <h5 className="card-title">
+                                                        {post.title}
+                                                    </h5>
+                                                    <p className="card-text">
+                                                        {post.body}
+                                                    </p>
+                                                    <div className="card-footer border-0 d-flex w-100 justify-content-between text-muted">
+                                                        <p>{getRandomDays()}</p>
+                                                        <div className='d-inline'>
+                                                            <div className="badge rounded-pill text-bg-light d-flex align-items-center">
+                                                                <span className='material-icons'>
+                                                                    thumb_up
+                                                                </span>23
+                                                            </div>
+                                                            <div className="mt-1 badge rounded-pill text-bg-light d-flex align-items-center">
+                                                                <span className='material-icons'>
+                                                                    question_answer
+                                                                </span>12
+                                                            </div>
                                                         </div>
                                                     </div>
                                                 </div>
                                             </div>
                                         </div>
                                     </div>
-                                </div>
-                            )
-                        }
+                                )
+                            }
+                        </div>
                     </div>
                 </div>
                 {
